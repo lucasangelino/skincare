b5b891ecd08bc6d1725c89cbd93efc5f52fa33ba
lucasangelino <angelino.n.lucas@gmail.com>
2022-02-16 17:42:38 -0300

    CHA add backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
 import * as React from 'react'
 
 import Login from './pages/login'
-import Layout from './components/core/Layout'
 function App() {
 
   return (
     <div className="App">
-      <Layout>
-        <h1>layout</h1>
-      </Layout>
+      <Login />
     </div>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
       <App />
-    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
