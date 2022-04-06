511f19e2fbb08a31dd47e93d2ba48fcacc626666
Lucas Angelino <angelino.n.lucas@gmail.com>
2022-04-06 19:18:56 -0300

    CHA center container

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -15,34 +15,41 @@ export default function Chat() {
   const { auth, logout } = React.useContext(AuthContext);
   const { chatState } = React.useContext(ChatContext);
   return (
-    <Container maxW="container.xl" bg={"#111B21"} h={"100vh"}>
-      <Grid
-        h="100vh"
-        templateRows="repeat(1, 1fr)"
-        templateColumns="repeat(3, 1fr)"
+    <Box bg="#0A1014" height={"100vh"} display={"flex"} alignItems={"center"}>
+      <Container
+        maxW="container.xl"
+        bg={"#111B21"}
+        height={"95vh"}
+        borderRadius={5}
       >
-        <GridItem colSpan={1}>
-          <VStack
-            divider={<StackDivider borderColor="gray.200" />}
-            align="stretch"
-          >
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <h1 style={{ color: "white" }}>{auth.name}</h1>
-              <h1 style={{ color: "white" }} onClick={logout}>
-                Salir
-              </h1>
-            </div>
-            {chatState.users
-              .filter((user) => user.id !== auth.uid)
-              .map((user) => (
-                <ChatItem key={user.id} user={user} />
-              ))}
-          </VStack>
-        </GridItem>
-        <GridItem colSpan={2}>
-          {chatState.activeChat ? <ChatViewer /> : <NoSelectedChat />}
-        </GridItem>
-      </Grid>
-    </Container>
+        <Grid
+          h="100vh"
+          templateRows="repeat(1, 1fr)"
+          templateColumns="repeat(3, 1fr)"
+        >
+          <GridItem colSpan={1}>
+            <VStack
+              divider={<StackDivider borderColor="gray.200" />}
+              align="stretch"
+            >
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h1 style={{ color: "white" }}>{auth.name}</h1>
+                <h1 style={{ color: "white" }} onClick={logout}>
+                  Salir
+                </h1>
+              </div>
+              {chatState.users
+                .filter((user) => user.id !== auth.uid)
+                .map((user) => (
+                  <ChatItem key={user.id} user={user} />
+                ))}
+            </VStack>
+          </GridItem>
+          <GridItem colSpan={2}>
+            {chatState.activeChat ? <ChatViewer /> : <NoSelectedChat />}
+          </GridItem>
+        </Grid>
+      </Container>
+    </Box>
   );
 }
