1d3a9220eb7be45bc1e00ae7da73600f6fce0cdb
Lucas Angelino <angelino.n.lucas@gmail.com>
2022-04-06 21:11:12 -0300

    CHA add avatar and name to chatviewer

--- frontend/src/components/framework/ChatViewer.jsx
+++ frontend/src/components/framework/ChatViewer.jsx
@@ -7,6 +7,7 @@ import {
   Text,
   Input,
   Button,
+  Avatar,
 } from "@chakra-ui/react";
 import { Container } from "@chakra-ui/react";
 import { Message } from "../framework/Message";
@@ -35,21 +36,37 @@ export default function ChatViewer() {
   };
 
   return (
-    <VStack justify={"space-between"} h={"100%"}>
-      <Container maxW="container.md" resize={false}>
-        {chatState.messages.map((message) => {
-          return <Message message={message} key={message._id} />;
-        })}
-      </Container>
-      <Button onClick={handleClick}>Enviar</Button>
-      <Input
-        value={message}
-        name="message"
-        color="#fff"
-        placeholder="large size"
-        size="lg"
-        onChange={handleChange}
-      />
-    </VStack>
+    <>
+      <Box bg={"#272D38"}>
+        <HStack justifyContent={"space-between"} p={2}>
+          <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+            <Avatar
+              size="md"
+              name="Kent Dodds"
+              src="https://bit.ly/kent-c-dodds"
+            />
+            <Text fontSize={"lg"} color={"gray.300"} marginLeft={3}>
+              Lucas
+            </Text>
+          </Box>
+        </HStack>
+      </Box>
+      <VStack justify={"space-between"} h={"100%"}>
+        <Container maxW="container.md" resize={false}>
+          {chatState.messages.map((message) => {
+            return <Message message={message} key={message._id} />;
+          })}
+        </Container>
+        <Button onClick={handleClick}>Enviar</Button>
+        <Input
+          value={message}
+          name="message"
+          color="#fff"
+          placeholder="large size"
+          size="lg"
+          onChange={handleChange}
+        />
+      </VStack>
+    </>
   );
 }
