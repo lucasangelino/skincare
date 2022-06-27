29084f6d203414f6fd602373131d30e78ae116ad
Lucas Angelino <angelino.n.lucas@gmail.com>
2022-06-27 15:59:06 -0300

    Change docker config

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,12 +1,12 @@
-const { Pool } = require('pg')
+const { Pool } = require("pg");
 
 const pg_pool = new Pool({
-  user: 'skincare',
-  host: 'localhost',
-  database: 'skincare',
-  password: '1234',
+  user: "skincare",
+  host: "skincare.pg",
+  database: "skincare",
+  password: "1234",
   port: 5432,
-})
+});
 
 module.exports = {
   pg_pool,
